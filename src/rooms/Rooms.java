26632dde4baaf58174a594176ae8290438336bb8
NikolasCCT <nikolasbholst@gmail.com>
2026-09-23 09:32:20 +0200

    Added rooms folder/class

---
+++ src/rooms/Rooms.java
@@ -0,0 +1,10 @@
+public class Rooms {
+
+    private string name;
+    private string description;
+
+    private Room north;
+    private Room east;
+    private Room south;
+    private Room west;
+}
