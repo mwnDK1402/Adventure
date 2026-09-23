adf7a39feb86b8a177f0669c608b5386ada5520e
NikolasCCT <nikolasbholst@gmail.com>
2026-09-23 09:52:56 +0200

    Added Getters, Setters & Variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+import Room.Room;
+
 public class Main {
 
-    static void main(String[] args) {
-        
+    static void main (String[] args) {
+        Room room1 = new Room("Room 1", "An empty cave");
+        Room room2 = new Room("Room 2", "New room 2");
+
+        Room currentRoom = new Room("","");
+
+        room1.setWest(room2);
+        room2.setEast(room1);
+
+
+
+        Room next = currentRoom.getNorth();
     }
 }
