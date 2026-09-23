f6719a91fd94a89e2ed83da4542cae17dd0bbec8
NikitaM2527 <nima1003@stud.ek.dk>
2026-09-23 10:40:57 +0200

    Added UserInterface.java

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class UserInterface
+{
+    public void main(String[] args)
+    {
+        Adventure adventure = new Adventure();
+
+        System.out.println("");
+    }
+}
