12e40e1e649ec4d8e60e5feb3101074d263b7c81
NikitaM2527 <nima1003@stud.ek.dk>
2026-09-23 10:58:28 +0200

    Add welcome to UserInterface.java

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 public class UserInterface
 {
-    public void main(String[] args)
+    public static void main(String[] args)
     {
+        Scanner scanner = new Scanner(System.in);
         Adventure adventure = new Adventure();
 
-        System.out.println("");
+        System.out.println("Welcome");
+        System.out.println();
+        System.out.println(adventure.look());
+        System.out.println();
+        System.out.print("Where do you want to go?: ");
+        String input = scanner.nextLine();
     }
 }
