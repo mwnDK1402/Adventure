2ec4445b8279bbaa4769bc03df029233400ed43c
NikolasCCT <nikolasbholst@gmail.com>
2026-09-23 11:37:39 +0200

    Finished room navigation

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -9,20 +9,20 @@ public class Adventure
     {
 
         Room room1 = new Room("Room 1", "An empty cave");
-        Room room2 = new Room("Room 2", "New room 2");
-        Room room3 = new Room("","");
-        Room room4 = new Room("","");
-        Room room5 = new Room("","");
-        Room room6 = new Room("","");
-        Room room7 = new Room("","");
-        Room room8 = new Room("","");
-        Room room9 = new Room("","");
+        Room room2 = new Room("Room 2", "Description 2");
+        Room room3 = new Room("Room 3","Description 3");
+        Room room4 = new Room("Room 4","Description 4");
+        Room room5 = new Room("Room 5","Description 5");
+        Room room6 = new Room("Room 6","Description 6");
+        Room room7 = new Room("Room 7","Description 7");
+        Room room8 = new Room("Room 8","Description 8");
+        Room room9 = new Room("Room 9","Description 9");
 
         currentRoom = room1;
 
-        room1.setWest(room2);
+        room1.setEast(room2);
         room1.setSouth(room4);
-        room2.setEast(room1);
+        room2.setWest(room1);
         room2.setEast(room3);
         room3.setWest(room2);
         room3.setSouth(room6);
@@ -46,16 +46,19 @@ public class Adventure
         return currentRoom.getDescription();
     }
 
+    public String getCurrentRoom() {
+        return currentRoom.getName();
+    }
+
     public Room moveNorth()
     {
         Room next = currentRoom.getNorth();
 
-        if (next != null)
-        {
+        if (next != null) {
             currentRoom = next;
             return currentRoom;
-        } else
-        {
+
+        } else {
             return null;
         }
     }
@@ -64,12 +67,11 @@ public class Adventure
     {
         Room next = currentRoom.getEast();
 
-        if (next != null)
-        {
+        if (next != null) {
             currentRoom = next;
             return currentRoom;
-        } else
-        {
+
+        } else {
             return null;
         }
     }
@@ -78,12 +80,11 @@ public class Adventure
     {
         Room next = currentRoom.getSouth();
 
-        if (next != null)
-        {
+        if (next != null) {
             currentRoom = next;
             return currentRoom;
-        } else
-        {
+
+        } else {
             return null;
         }
     }
@@ -92,12 +93,11 @@ public class Adventure
     {
         Room next = currentRoom.getWest();
 
-        if (next != null)
-        {
+        if (next != null) {
             currentRoom = next;
             return currentRoom;
-        } else
-        {
+
+        } else {
             return null;
         }
     }
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,3 +1,6 @@
+import Room.Room;
+
+import java.util.Locale;
 import java.util.Scanner;
 
 public class UserInterface
@@ -7,11 +10,58 @@ public class UserInterface
         Scanner scanner = new Scanner(System.in);
         Adventure adventure = new Adventure();
 
-        System.out.println("Welcome");
-        System.out.println();
-        System.out.println(adventure.look());
-        System.out.println();
-        System.out.print("Where do you want to go?: ");
-        String input = scanner.nextLine();
+        System.out.println("Welcome to the maze!\n");
+        System.out.println("You're currently in " + adventure.getCurrentRoom() + ": " + adventure.look());
+        System.out.print("\nWhere do you want to go?");
+
+        String input;
+
+        do {
+            System.out.println("\nInput: ");
+            input = scanner.nextLine().toLowerCase();
+
+            switch (input) {
+                case "north" -> {
+                    Room nextRoom = adventure.moveNorth();
+
+                    if (nextRoom == null) {
+                        System.out.println("You cannot go that way.");
+                    } else {
+                        System.out.println("You are in " + nextRoom.getName());
+                        System.out.println(adventure.look());
+                    }
+                }
+                case "east" -> {
+                    Room nextRoom = adventure.moveEast();
+
+                    if (nextRoom == null) {
+                        System.out.println("You cannot go that way.");
+                    } else {
+                        System.out.println("You are in " + nextRoom.getName());
+                        System.out.println(adventure.look());
+                    }
+                }
+                case "south" -> {
+                    Room nextRoom = adventure.moveSouth();
+                    if (nextRoom == null) {
+                        System.out.println("You cannot go that way.");
+                    } else {
+                        System.out.println("You are in " + nextRoom.getName());
+                        System.out.println(adventure.look());
+                    }
+                }
+                case "west" -> {
+                    Room nextRoom = adventure.moveWest();
+                    if (nextRoom == null) {
+                        System.out.println("You cannot go that way.");
+                    } else {
+                        System.out.println("You are in " + nextRoom.getName());
+                        System.out.println(adventure.look());
+                    }
+                }
+            }
+        } while (!input.equals("exit")); {
+            System.out.println("You're exiting the maze...");
+        }
     }
 }
