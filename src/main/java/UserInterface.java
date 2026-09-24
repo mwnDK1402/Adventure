d3f580a32335760298f4b003c9f2966e7f9a5e3b
NikitaM2527 <nima1003@stud.ek.dk>
2026-09-24 13:45:12 +0200

    Use master switch instead of per-command switch

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class UserInterface
 {
+    private static String input;
+    private static Adventure adventure;
+
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        Adventure adventure = new Adventure();
+        adventure = new Adventure();
 
         System.out.println("Welcome to the maze!\n");
         System.out.println("You're currently in " + adventure.getCurrentRoom().getName() + ": " + adventure.look());
         System.out.print("\nWhere do you want to go?");
-
-        String input;
 
         while (true) {
             System.out.print("\nInput: ");
@@ -21,29 +22,31 @@ public class UserInterface
 
             if (input.equals("exit")) break;
 
-            MoveResult moveResult = switch (input) {
-                case "north" -> adventure.moveNorth();
-                case "east" -> adventure.moveEast();
-                case "south" -> adventure.moveSouth();
-                case "west" -> adventure.moveWest();
-                default -> null;
-            };
-
-            switch (moveResult) {
-                case EnteredRoom -> {
-                    Room room = adventure.getCurrentRoom();
-                    System.out.println("You are in " + room.getName());
-                    if (!room.isVisited()) {
-                        System.out.println(room.getDescription());
-                        room.setVisited();
-                    }
-                }
-                case HitWall -> System.out.println("You cannot go that way.");
-                case DoorLocked -> System.out.println("The door is locked.");
-                case null -> System.out.printf("%s is not a valid direction", input);
+            switch (input) {
+                case "go north", "move north" -> performMove(adventure.moveNorth());
+                case "go east", "move east" -> performMove(adventure.moveEast());
+                case "go south", "move south" -> performMove(adventure.moveSouth());
+                case "go west", "move west" -> performMove(adventure.moveWest());
+                case "look" -> System.out.println(adventure.look());
             }
         }
 
         System.out.println("You're exiting the maze...");
+    }
+
+    private static void performMove(MoveResult moveResult) {
+        switch (moveResult) {
+            case EnteredRoom -> {
+                Room room = adventure.getCurrentRoom();
+                System.out.println("You are in " + room.getName());
+                if (!room.isVisited()) {
+                    System.out.println(room.getDescription());
+                    room.setVisited();
+                }
+            }
+            case HitWall -> System.out.println("You cannot go that way.");
+            case DoorLocked -> System.out.println("The door is locked.");
+            case null -> System.out.printf("%s is not a valid direction", input);
+        }
     }
 }
